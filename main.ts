0c1cf8a5b4080ed6e8d86ac7229c17dbcfe4bc3f
unknown <xingguang64@yeah.net>
2024-09-08 20:34:00 +0800

    数组的高级用法1

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
 import useArray from "./src/Array/useArray";
 
-const { createValues, createRandomValues, createRange } = useArray();
+const {
+  createValues,
+  createRandomValues,
+  createRange,
+  uniqueArray,
+  intersection,
+  removeFalsyValues,
+  max,
+  min,
+} = useArray();
 
-const values = createRange(1, 100, 3);
+const arr3 = [1, "", 0, null, undefined, "test", NaN, true];
+const values = createRandomValues(10);
+const values2 = createRandomValues(10);
+const arr = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }, { a: 5 }, { a: 1 }];
+const arr2 = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }, { a: 5 }, { a: 6 }];
+console.log(uniqueArray(arr, "a"));
 
 console.log(values);
+console.log(values2);
+console.log(uniqueArray(values));
+console.log(intersection(arr, arr2, "a"));
+console.log(intersection(values, values2));
+console.log(removeFalsyValues(arr3));
+console.log(max(values));
+console.log(min(values));
--- src/Array/useArray.ts
+++ src/Array/useArray.ts
@@ -8,7 +8,7 @@ function useArray() {
   }
 
   // 随机生成多少范围内的数据整数生成器
-  function createRandomValues(inside: number, length: number = 10) {
+  function createRandomValues(inside: number, length: number = 10): number[] {
     return createValues((_, index) => (Math.random() * inside) | 0, length);
   }
 
@@ -20,10 +20,76 @@ function useArray() {
     );
   }
 
+  // 数据去重
+  function uniqueArray<T>(arr: T[], key?: string | number) {
+    if (key == null) {
+      return Array.from(new Set(arr));
+    }
+
+    const keyValues = new Set();
+    let val: T;
+    return arr.filter((obj) => {
+      val = obj[key];
+      if (keyValues.has(val)) {
+        return false;
+      }
+      keyValues.add(val);
+      return true;
+    });
+  }
+
+  // 求交集
+  function intersection<T>(arr1: T[], arr2: T[], key?: string | number): T[] {
+    const map = new Map();
+
+    // 基本数据类型
+    if (key == null) {
+      arr1.forEach((item) => map.set(item, null));
+      return arr2.filter((item) => map.has(item));
+    }
+
+    // 引用数据类型
+    arr1.forEach((item) => map.set(item[key], null));
+    return arr2.filter((item) => map.has(item[key]));
+  }
+
+  // 求差集
+  function difference<T>(arr1: T[], arr2: T[], key?: string | number): T[] {
+    const map = new Map();
+    if (key == null) {
+      arr1.forEach((item) => map.set(item, null));
+      return arr2.filter((item) => !map.has(item));
+    }
+
+    arr1.forEach((item) => map.set(item[key], null));
+    return arr2.filter((item) => !map.has(item[key]));
+  }
+
+  // 从数组中删除虚假值
+  function removeFalsyValues(arr: any[]) {
+    return arr.filter(Boolean);
+  }
+
+  // 最大值
+  function max(arr: number[]) {
+    return Math.max.apply(Math, arr);
+  }
+
+  // 最小值
+  function min(arr: number[]) {
+    return Math.min.apply(Math, arr);
+  }
+
   return {
     createValues,
     createRandomValues,
     createRange,
+    uniqueArray,
+    intersection,
+    difference,
+    removeFalsyValues,
+    max,
+    min,
   };
 }
 
